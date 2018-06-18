33735502c169892e4c1d8facfca96e3f8e65027f
harshalsolao <harshalsolao@gmail.com>
2018-06-18 22:43:01 +0530

    Initial Project Version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MyserviceService } from './myservice.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'Angular 4 Project!!';
+  months = ['January', 'Feburary', 'March', 'April', 'May',
+  'June', 'July', 'August', 'September',
+  'October', 'November', 'December'];
+  isavailable = true;
+  // todayDate = new Date();
+  todaydate;
+  componentproperty;
+  jsonval = {name: 'Harshal', age: '28', address: {a1: 'Pune', a2: 'Maharashtra'}};
+  changemonths(event) {
+    alert('Changed month from the Dropdown');
+    console.log('Changed month from the Dropdown');
+    console.log(event);
+   }
+  myClickFunction(event) {
+    alert('Button is clicked');
+    console.log(event);
+ }
+ constructor(private myservice: MyserviceService) {}
+ ngOnInit() {
+  this.todaydate = this.myservice.showTodayDate();
+  console.log(this.myservice.serviceproperty);
+      this.myservice.serviceproperty = 'component created'; // value is changed.
+      this.componentproperty = this.myservice.serviceproperty;
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule} from '@angular/router';
 
+import { MyserviceService } from './myservice.service';
 import { AppComponent } from './app.component';
+import { NewCmpComponent } from './new-cmp/new-cmp.component';
+import { ChangeTextDirective } from './change-text.directive';
+import { SqrtPipe } from './app.sqrt';
 
 @NgModule({
   declarations: [
-    AppComponent
+    SqrtPipe,
+    AppComponent,
+    NewCmpComponent,
+    ChangeTextDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+         path: 'new-cmp',
+         component: NewCmpComponent
+      }
+   ])
   ],
-  providers: [],
+  providers: [MyserviceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
